05c8a49b5e77b9ed706950dc5df820fdea5a6e68
theliving22 <michael_belding@yahoo.com>
2016-04-16 15:16:01 -0400

    the Basic outline of the game. Make changes if you fill necessary to do so. If not come into the lab and we'll discuss it.

--- src/csu/csci325/GameBehindTheScenes.java
+++ src/csu/csci325/GameBehindTheScenes.java
@@ -1,7 +1,48 @@
 package csu.csci325;
 
+import javax.swing.*;
+
 /**
  * Created by student on 4/16/2016.
  */
-public class GameBehindTheScenes {
+public class GameBehindTheScenes
+{
+    private JButton north = new JButton("North");
+    private JButton south = new JButton("South");
+    private JButton east = new JButton("East");
+    private JButton west = new JButton("West");
+    private JLabel theLabel = new JLabel("Hi Welcome");
+    private JFrame theFrame = new JFrame();
+    private JPanel thePannel = new JPanel();
+
+    public GameBehindTheScenes()
+    {
+        thePannel.add(north);
+        thePannel.add(south);
+        thePannel.add(east);
+        thePannel.add(west);
+        thePannel.add(theLabel);
+
+        theFrame.add(thePannel);
+        theFrame.setVisible(true);
+        theFrame.setSize(200,150);
+
+
+    }
+
+    public JFrame getTheFrame()
+    {
+        return theFrame;
+    }
+
+
+    public static void main(String[] args) {
+
+        GameBehindTheScenes theScenes = new GameBehindTheScenes();
+        theScenes.getTheFrame();
+
+    }
+
+
+
 }
