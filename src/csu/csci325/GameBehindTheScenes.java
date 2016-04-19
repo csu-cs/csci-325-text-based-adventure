a8e7274fae5397e178d8b8c78e9bbac9a4f30cc6
theliving22 <michael_belding@yahoo.com>
2016-04-19 10:24:06 -0400

    the Basic outline of the game. Make changes if you fill necessary to do so. If not come into the lab and we'll discuss it.

--- src/csu/csci325/GameBehindTheScenes.java
+++ src/csu/csci325/GameBehindTheScenes.java
@@ -1,45 +1,130 @@
 package csu.csci325;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- * Created by student on 4/16/2016.
- */
 public class GameBehindTheScenes
 {
-    private JButton north = new JButton("North");
-    private JButton south = new JButton("South");
-    private JButton east = new JButton("East");
-    private JButton west = new JButton("West");
-    private JLabel theLabel = new JLabel("Hi Welcome");
-    private JFrame theFrame = new JFrame();
-    private JPanel thePannel = new JPanel();
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JLabel theLabel;
+    private JFrame theFrame;
+    private JPanel thePanel;
 
     public GameBehindTheScenes()
     {
-        thePannel.add(north);
-        thePannel.add(south);
-        thePannel.add(east);
-        thePannel.add(west);
-        thePannel.add(theLabel);
 
-        theFrame.add(thePannel);
+    }
+
+    public void startOfGame()
+    {
+        button1 = new JButton("North");
+        button2 = new JButton("South");
+        button3 = new JButton("East");
+        button4 = new JButton("West");
+        thePanel = new JPanel();
+        theFrame = new JFrame();
+        theLabel = new JLabel("Welcome to the Game.");
+
+        thePanel.add(button1);
+        thePanel.add(button2);
+        thePanel.add(button3);
+        thePanel.add(button4);
+        thePanel.add(theLabel);
+
+
+        theFrame.add(thePanel);
+        theFrame.setVisible(true);
+        theFrame.setSize(200, 150);
+    }
+
+    public void yesOrNoOption()
+    {
+
+        button1 = new JButton("Yes");
+        button2 = new JButton("No");
+        thePanel = new JPanel();
+        theFrame = new JFrame();
+
+        thePanel.add(button1);
+        thePanel.add(button2);
+
+        theFrame.add(thePanel);
         theFrame.setVisible(true);
         theFrame.setSize(200,150);
 
-
     }
 
-    public JFrame getTheFrame()
+    public void secondScenario()
     {
-        return theFrame;
+
     }
+        public void displayScenario() {
+
+            button1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    theFrame.dispose();
+                    yesOrNoOption();
+                    theLabel = new JLabel("Welcome to north!");
+                    thePanel.add(theLabel);
+
+                    button1.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            secondScenario();
+                        }
+                    });
+                    button2.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            startOfGame();
+                        }
+                    });
+                }
+            });
+            button2.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    theFrame.dispose();
+                    yesOrNoOption();
+                    theLabel = new JLabel("Welcome to South!");
+                    thePanel.add(theLabel);
+                }
+            });
+            button3.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    theFrame.dispose();
+                    yesOrNoOption();
+                    theLabel = new JLabel("Welcome to East!");
+                    thePanel.add(theLabel);
+                }
+            });
+            button4.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    theFrame.dispose();
+                    yesOrNoOption();
+                    theLabel = new JLabel("Welcome to West!");
+                    thePanel.add(theLabel);
+                }
+            });
+        }
+
+
 
 
     public static void main(String[] args) {
 
         GameBehindTheScenes theScenes = new GameBehindTheScenes();
-        theScenes.getTheFrame();
+
+        theScenes.startOfGame();
+        theScenes.displayScenario();
 
     }
 
